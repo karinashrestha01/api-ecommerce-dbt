b5b2431361b9324c00f7d0c035aafdf0890f29fd
karinashrestha01 <karinashrestha@lftechnology.com>
2026-01-20 12:58:04 +0545

    feat/Updated on seller

--- ecom_transformation/models/marts/dim_seller.sql
+++ ecom_transformation/models/marts/dim_seller.sql
@@ -4,10 +4,7 @@ WITH stg_sellers AS (
 
 SELECT 
     seller_id,
-    -- FIX: Use 'zip_code' from staging
     zip_code AS seller_zip_code,
-    -- FIX: Use 'city' from staging
-    city AS seller_city,
-    -- FIX: Use 'state' from staging
+    seller_city AS seller_city,
     state AS seller_state
 FROM stg_sellers
